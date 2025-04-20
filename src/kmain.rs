512bb3eb35c95f194bc06c8227c9972ffd0f3205
senseisamy <samy.naji2003@gmail.com>
2025-04-21 01:34:33 +0200

    feat: added vga text writer

--- src/kmain.rs
+++ src/kmain.rs
@@ -1,28 +1,18 @@
 #![no_std]
 #![no_main]
 
+mod vga_buffer;
 use core::panic::PanicInfo;
-
-const VGA_BUFFER: *mut u8 = 0xb8000 as *mut u8;
-// const VGA_WIDTH: usize = 80;
-// const VGA_HEIGHT: usize = 25;
 
 #[unsafe(no_mangle)]
 pub extern "C" fn k_main() -> ! {
-	let message = "42";
-	
-	// Print the message to the VGA text buffer
-	for (i, &byte) in message.as_bytes().iter().enumerate() {
-		unsafe {
-			*VGA_BUFFER.add(i * 2) = byte;
-			*VGA_BUFFER.add(i * 2 + 1) = 0x0F;
-		}
-	}
+	println!("42");
 
 	loop {}
 }
 
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+fn panic(info: &PanicInfo) -> ! {
+	println!("{info}");
 	loop {}
 }
